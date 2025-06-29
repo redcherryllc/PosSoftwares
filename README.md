5397c470c6b9cc9cd63b57893d6dcb8724b0ba87
Anju Rajan <jegan18004@gmail.com>
2025-06-29 18:51:32 +0530

    value change in  local host

---
+++ README.md
@@ -0,0 +1,13 @@
+# My Django Project
+
+This is a Django-based web application for managing fuel sales.
+
+## Features
+- Admin and Salesperson roles
+- POS system
+- Reporting and billing
+
+## How to Run
+
+```bash
+python manage.py runserver
--- demomess/settings.py
+++ demomess/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'django-insecure-a7+j7@461#0lznh$(4pr-8$7j6%p+*2(v6)8h%7y_wf%k$p)9+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['3.87.5.72', 'pos.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
