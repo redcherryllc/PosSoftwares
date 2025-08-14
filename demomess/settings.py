774e8b44d79c153afdbd50e73f403a728167bfe7
Anju Rajan <jegan18004@gmail.com>
2025-08-14 12:44:35 +0530

    save updated files

--- demomess/settings.py
+++ demomess/settings.py
@@ -139,7 +139,7 @@ STATIC_ROOT = "/static/"
 #STATIC_URL = '/static/'
 #STATICFILES_DIRS = [BASE_DIR / 'static']
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
